9416f9a5aae66b686b32d9fe0a6794a35132b542
Valentyner <robertdoms2002@gmail.com>
2023-04-13 12:44:52 +0300

    pz_1.13_08

--- Pz_1.13/pz_1.13_08.js
+++ Pz_1.13/pz_1.13_08.js
@@ -33,19 +33,28 @@ class ExecutableTask extends Task {
     }
 
     descriptionOfTask() {
-        console.log("Ваша задача :" + ` ${this.property1}` + "\nОпис задачі :" + ` ${this.property2}` + "\nПочаток :" + ` ${this.property3}` + "\nКінець :" + ` ${this.property4}` + "\nВідсоток виконання : " + `${this.progress} %` + "\n" + this.setCompletionPercentage(this.progress))
+        console.log("Ваша задача :" + ` ${this.property1}` + "\nОпис задачі :" + ` ${this.property2}` + "\nПочаток :" + ` ${this.property3}` + "\nКінець :" + ` ${this.property4}` + "\nВідсоток виконання : " + `${this.progress} %` + "\n" + this.setCompletionPercentage(this.progress) + "\n")
 
-        console.log("Ваші підзадачі :")
+        this.descriptionOfSubtasks();
+        
+    }
+
+    descriptionOfSubtasks() {
+        console.log("--------SUBTASKS--------")
         for(let i = 0; i < this.subtasks.length; i ++) {
-            console.log(i + 1 + " " + this.subtasks[i])
+            console.log("Ваша задача :" + ` ${this.subtasks[i].property1}` + "\nОпис задачі :" + ` ${this.subtasks[i].property2}` + "\nПочаток :" + ` ${this.subtasks[i].property3}` + "\nКінець :" + ` ${this.subtasks[i].property4}` + "\nВідсоток виконання : " + `${this.subtasks[i].progress} %` + "\n" + this.setCompletionPercentage(this.subtasks[i].progress) + "\n")
         }
     }
 
 }
 
 const task = new ExecutableTask("Випускний;", "Святкувати випускний;", new Date(2021, 6, 26), new Date(2021, 7, 27));
-task.addSubtask("Повернутися додому");
-task.addSubtask("Не получити по шапці за те що п'яний");
+const subtask1 = new ExecutableTask("Повернутися додому", "---", new Date(2021, 7, 26, 15, 30), new Date(2021, 7, 27, 6, 30))
+const subtask2 = new ExecutableTask("Не получити по шапці за те що п'яний", "Майже неможливо", new Date(2021, 7, 26, 15, 30), new Date(2021, 7, 27, 6, 30))
+task.addSubtask(subtask1);
+task.addSubtask(subtask2);
+subtask1.setCompletionPercentage(30);
+subtask2.setCompletionPercentage(40);
 
 task.setCompletionPercentage(100);
 
